a6d82000ce3bd42e79e1f3e93fd6fea1677bb9a3
Lucasmarea <lucasdvd2@gmail.com>
2021-01-21 19:45:42 -0300

    210121

--- Arquivos do curso/exercicio5.php
+++ Arquivos do curso/exercicio5.php
@@ -1,6 +1,6 @@
 <?php
 
-$salario = 2.500;
+$salario = 5.500;
 $dependentes = 3;
 
 function calcIRPF($salario, $dependentes) 
@@ -9,20 +9,26 @@ function calcIRPF($salario, $dependentes)
 
         $imposto =(($salario -($dependentes * 189.59))* (0/100)) - $dedução;
 
-    }else if ($salario = 1.903 <=> 2.826) {
+        echo $imposto;
 
-       // $imposto =(($salario -($dependentes * 189.59))* (7,5/100)) - $dedução;
+    }else if ($salario = 1.903 <=> 2.826) {}
 
-    }  else ($salario = 2.826 <=> 3.751){
+       $imposto =(($salario -($dependentes * 189.59))* (7.5/100)) - $dedução;
+       echo $imposto;
+
+{  if ($salario = 2.826 <=> 3.751){
 
         $imposto =(($salario -($dependentes * 189.59))* (15/100)) - $dedução;
-    } else ($salario = 3.751 <=> 4.664){
+        echo $imposto;
+    }  else if($salario = 3.751 <=> 4.664){}
 
-        $imposto =(($salario -($dependentes * 189.59))* (22,5/100)) - $dedução;
+        $imposto =(($salario -($dependentes * 189.59))* (22.5/100)) - $dedução;
+        echo $imposto;
 
-    } else ($salario > 4.664)
+{ if($salario > 4.664)
 
-         $imposto =(($salario -($dependentes * 189.59))* (27,5/100)) - $dedução;
+         $imposto =(($salario -($dependentes * 189.59))* (27.5/100)) - $dedução;}
+         echo $imposto;
 }
 
-//somar();
+}
